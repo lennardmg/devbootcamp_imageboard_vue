984cf51280eaed81260aedc3ea8e2ad4c0ee6c8e
lennardmg <lennardmg@gmx.de>
2022-10-13 16:35:05 +0200

    layout is done, before implementing the upload functionality I want to push once to be on the safe side.

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,58 @@
 import * as Vue from './vue.js';
 
+
+
 Vue.createApp({
+
 data() {
+
     return {
         title: "Lennard's Image Board 🏖️",
         images: [
             {
                 src: "https://picsum.photos/200",
                 title: "Title1",
+                description: "random description1",
             },
             {
                 src: "https://picsum.photos/300",
                 title: "Title2",
+                description: "random description2",
             },
             {
                 src: "https://picsum.photos/400",
                 title: "Title3",
+                description:
+                    "very very very very very very veryvery very very very long description",
+            },
+            {
+                src: "https://picsum.photos/500",
+                title: "very long title that probably exceeds the field given under the image ...",
+                description: "",
+            },
+            {
+                src: "https://picsum.photos/600",
+                title: "Title6",
             },
         ],
     };
 }
 }).mount("#main");
+
+
+
+// uploadImage() {
+//     const file = document.querySelector("input[type=file]").files[0]
+//     const formData = new FormData();
+
+//     formData.append("file", file);
+
+//     fetch("/images", {
+//         method: "POST",
+//         body: formData
+//     })
+// }
+
+/////////////// needs to be extended with username, title, description etc. //////////////
+//////// plus after success, we need to append the newly created image into the images list, eg.
+/////////// already create a json and put it into the image array /////////
--- server.js
+++ server.js
@@ -7,8 +7,52 @@ const { PORT = 8080 } = process.env;
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
+/////////// for uploading files to the server: /////////////
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+const multer = require("multer");
+const uidSafe = require("uid-safe");
+const path = require("path");
+
+const diskStorage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, path.join(__dirname, "uploads"));
+    },
+    filename: function (req, file, callback) {
+        uidSafe(24).then(function (uid) {
+            callback(null, uid + path.extname(file.originalname));
+        });
+    },
+});
+
+const uploader = multer({
+    storage: diskStorage,
+    limits: {
+        fileSize: 2097152,
+    },
+});
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
+
+
+app.post("/images", uploader.single("file"), function (req, res) {
+    // If nothing went wrong the file is already in the uploads directory
+    if (req.file) {
+        res.json({
+            success: true,
+        });
+    } else {
+        res.json({
+            success: false,
+        });
+    }
+});
+
 
 app.listen(PORT, () => console.log(`I'm listening on port ${PORT}`));
